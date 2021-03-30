c9e31da24334e415781c17d5d7259a6f0621050f
lucasrpb <lucasrpb@gmail.com>
2021-03-30 10:31:41 -0300

    Some fixings...

--- src/main/scala/services/scalable/pubsub/PubSubHelper.scala
+++ src/main/scala/services/scalable/pubsub/PubSubHelper.scala
@@ -1,15 +1,23 @@
 package services.scalable.pubsub
 
-import com.google.cloud.pubsub.v1.{SubscriptionAdminClient, TopicAdminClient, TopicAdminSettings}
+import com.google.cloud.pubsub.v1.{SubscriptionAdminClient, SubscriptionAdminSettings, TopicAdminClient, TopicAdminSettings}
 import com.google.pubsub.v1.{ListTopicsRequest, ProjectSubscriptionName, PushConfig, TopicName}
 import org.slf4j.LoggerFactory
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 object PubSubHelper {
 
   val logger = LoggerFactory.getLogger(this.getClass)
 
-  val subAdmin = SubscriptionAdminClient.create()
-  val topicAdmin = TopicAdminClient.create()
+  val subAdminSettings = SubscriptionAdminSettings.newBuilder()
+    .setCredentialsProvider(GOOGLE_CREDENTIALS_PROVIDER).build()
+
+  val topicAdminSettings = TopicAdminSettings.newBuilder()
+    .setCredentialsProvider(GOOGLE_CREDENTIALS_PROVIDER).build()
+
+  val subAdmin = SubscriptionAdminClient.create(subAdminSettings)
+  val topicAdmin = TopicAdminClient.create(topicAdminSettings)
 
   /*def listTopics(): Seq[String] = {
     var topics = Seq.empty[String]
@@ -37,33 +45,81 @@ object PubSubHelper {
     subscriptions
   }*/
 
-  def createTopics(topics: Map[String, TopicName]): Unit = {
+  def createTopics(topics: Map[String, TopicName])(implicit ec: ExecutionContext): Future[Boolean] = {
+    var tasks = Seq.empty[Future[Boolean]]
+
     topics.foreach { case (topic, name) =>
-      val topic = topicAdmin.createTopic(name)
-      logger.info(s"created topic: ${topic}")
+      val pr = Promise[Boolean]()
+
+      ec.execute{ () =>
+        val topic = topicAdmin.createTopic(name)
+        logger.info(s"created topic: ${topic}")
+
+        pr.success(true)
+      }
+
+      tasks = tasks :+ pr.future
     }
+
+    Future.sequence(tasks).map(!_.contains(false))
   }
 
-  def deleteTopics(topics: Seq[String]): Unit = {
+  def deleteTopics(topics: Seq[String])(implicit ec: ExecutionContext): Future[Boolean] = {
+    var tasks = Seq.empty[Future[Boolean]]
+
     topics.foreach { t =>
-      topicAdmin.deleteTopic(t)
-      logger.info(s"deleted topic: ${t}")
+      val pr = Promise[Boolean]()
+
+      ec.execute { () =>
+        topicAdmin.deleteTopic(t)
+        logger.info(s"deleted topic: ${t}")
+
+        pr.success(true)
+      }
+
+      tasks = tasks :+ pr.future
     }
+
+    Future.sequence(tasks).map(!_.contains(false))
   }
 
-  def createSubscriptions(subscriptions: Map[String, TopicName]): Unit = {
+  def createSubscriptions(subscriptions: Map[String, TopicName])(implicit ec: ExecutionContext): Future[Boolean] = {
+    var tasks = Seq.empty[Future[Boolean]]
+
     subscriptions.foreach { case (s, topicName) =>
-      val subscription = subAdmin.createSubscription(ProjectSubscriptionName.of(Config.projectId, s), topicName,
-        PushConfig.getDefaultInstance(), 10)
-      logger.info(s"created push subscription: ${subscription}\n")
+      val pr = Promise[Boolean]()
+
+      ec.execute { () =>
+        val subscription = subAdmin.createSubscription(ProjectSubscriptionName.of(Config.projectId, s), topicName,
+          PushConfig.getDefaultInstance(), 10)
+        logger.info(s"created push subscription: ${subscription}\n")
+
+        pr.success(true)
+      }
+
+      tasks = tasks :+ pr.future
     }
+
+    Future.sequence(tasks).map(!_.contains(false))
   }
 
-  def deleteSubscriptions(subscriptions: Seq[String]): Unit = {
+  def deleteSubscriptions(subscriptions: Seq[String])(implicit ec: ExecutionContext): Future[Boolean] = {
+    var tasks = Seq.empty[Future[Boolean]]
+
     subscriptions.foreach { s =>
-      subAdmin.deleteSubscription(s)
-      logger.info(s"deleted push subscription: ${s}\n")
+      val pr = Promise[Boolean]()
+
+      ec.execute { () =>
+        subAdmin.deleteSubscription(s)
+        logger.info(s"deleted push subscription: ${s}\n")
+
+        pr.success(true)
+      }
+
+      tasks = tasks :+ pr.future
     }
+
+    Future.sequence(tasks).map(!_.contains(false))
   }
 
 }
--- src/test/scala/services/scalable/pubsub/PubSubSpec.scala
+++ src/test/scala/services/scalable/pubsub/PubSubSpec.scala
@@ -2,12 +2,16 @@ package services.scalable.pubsub
 
 import org.scalatest.flatspec.AnyFlatSpec
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
 class PubSubSpec extends AnyFlatSpec {
 
   "it" should "create and delete topics" in {
 
-    PubSubHelper.createTopics(Config.topics)
-    PubSubHelper.createSubscriptions(Config.subscriptions)
+    Await.result(PubSubHelper.createTopics(Config.topics), Duration.Inf)
+    Await.result(PubSubHelper.createSubscriptions(Config.subscriptions), Duration.Inf)
 
   }
 
